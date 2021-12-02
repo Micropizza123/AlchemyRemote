f694d8f49900e3e428de7710336b4e93782e9057
Micropizza123 <leevi@DESKTOP-M76RJUV>
2021-12-02 20:00:14 +0200

    lol

--- src/main/java/net/mcreator/alchemy/procedures/ColdedlavabottleprocProcedure.java
+++ src/main/java/net/mcreator/alchemy/procedures/ColdedlavabottleprocProcedure.java
@@ -6,8 +6,10 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 
+import net.minecraft.world.server.ServerWorld;
 import net.minecraft.world.World;
 import net.minecraft.world.IWorld;
+import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.math.RayTraceContext;
 import net.minecraft.util.math.BlockPos;
@@ -18,7 +20,9 @@ import net.minecraft.item.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.effect.LightningBoltEntity;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.Entity;
 import net.minecraft.block.Blocks;
 
